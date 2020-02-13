3678e1b9b988e098cf1ab837998c67ea80617d36
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-02-13 22:03:05 +0000

    Remove unused imports

--- src/main/java/com/artemas/AccountSecurityApplication.java
+++ src/main/java/com/artemas/AccountSecurityApplication.java
@@ -2,18 +2,6 @@ package com.artemas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AccountSecurityApplication {
@@ -21,5 +9,4 @@ public class AccountSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountSecurityApplication.class, args);
 	}
-
 }
